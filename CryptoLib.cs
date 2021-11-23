8387a4e208af74c55a09a5d9821aa4c94a12a59f
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-11-23 15:31:53 +0300

    Task04

---
+++ CryptoLib.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Task04
+{
+    class CryptoLib
+    {
+        public static string HashBytesToString(byte[] hashBytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+        public static string CreateMD5(string input)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return HashBytesToString(hashBytes);
+        }
+
+        public static string CreateSHA1(string input)
+        {
+            using SHA1 sha1 = SHA1.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = sha1.ComputeHash(inputBytes);
+
+            return HashBytesToString(hashBytes);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Выведите платёжные ссылки для трёх разных систем платежа: 
+            //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
+            //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
+            //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+            
+            Order order = new Order(1234, 200);
+
+            PaymentSystem1 payment1 = new PaymentSystem1();
+            Console.WriteLine($"PaySystem1Link : {payment1.GetPayingLink(order)}");
+
+            PaymentSystem2 payment2 = new PaymentSystem2();
+            Console.WriteLine($"PaySystem2Link : {payment2.GetPayingLink(order)}");
+
+            PaymentSystem3 payment3 = new PaymentSystem3("SeCrEt-KEY-SyStEm3");
+            Console.WriteLine($"PaySystem3Link : {payment3.GetPayingLink(order)}");
+
+            Console.ReadLine();
+
+        }
+        public class Order
+        {
+            public readonly int Id;
+            public readonly int Amount;
+
+            public Order(int id, int amount) => (Id, Amount) = (id, amount);
+        }
+
+        public interface IPaymentSystem
+        {
+            public string GetPayingLink(Order order);
+        }
+
+        class PaymentSystem1 : IPaymentSystem
+        {
+
+            public string GetPayingLink(Order order)
+            {
+                //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
+                return $"pay.system1.ru/order?amount={order.Amount}RUB&hash={CryptoLib.CreateMD5(order.Id.ToString())}";
+            }
+        }
+
+        class PaymentSystem2 : IPaymentSystem
+        {
+            public string GetPayingLink(Order order)
+            {
+                //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
+                return $"order.system2.ru/pay?hash={CryptoLib.CreateMD5(order.Id.ToString() + order.Amount.ToString())}";
+            }
+        }
+        class PaymentSystem3 : IPaymentSystem
+        {
+            public readonly string SecretKey;
+
+            public PaymentSystem3(string secretKey)
+            {
+                SecretKey = secretKey;
+            }
+            public string GetPayingLink(Order order)
+            {
+                //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+                return $"system3.com/pay?amount={order.Amount}&curency=RUB&hash={CryptoLib.CreateSHA1(order.Amount.ToString() + order.Id.ToString() + SecretKey)}";
+            }
+        }
+    }
+}
+
+
+
