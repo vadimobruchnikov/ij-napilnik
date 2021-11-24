c6692d70e88f75a8ce0e8edb179532af0edaa809
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-11-25 00:49:37 +0300

    Task11 init (условие задачи)

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+// ЗАДАЧА 15 ИМЕНА КЛАССОВ И ОБЪЕКТОВ должны представлять собой существительные
+// Переименуйте классы - https://gist.github.com/HolyMonkey/301475a9d6ec900584a254cee3485673
+
+// Было
+
+class PlayerData { }
+class GunController { }
+class FollowTarget { }
+class GetUnit
+{
+    public IReadOnlyCollection<Unit> UnitsToGet {get; private set;}
+}
