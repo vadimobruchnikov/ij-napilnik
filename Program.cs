8adbd52b9a6bd61cf3f1043732a88841db1ff3fa
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-11-25 01:33:03 +0300

    Task13 solution

--- Program.cs
+++ Program.cs
@@ -3,15 +3,30 @@
 
 // Поправьте код - https://gist.github.com/HolyMonkey/228d407270b740387bbab0fede8fc29b
 
+class Weapon
+{
+    public float Cooldown { get; private set; }
+    public int Damage { get; private set; }
+
+    public bool IsReloading()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Movement
+{
+    public float DirectionX { get; private set; }
+    public float DirectionY { get; private set; }
+    public float Speed { get; private set; }
+}
+
 class Player
 {
     public string Name { get; private set; }
-    public float WeaponCooldown { get; private set; }
-    public float MovementDirectionX { get; private set; }
-    public float MovementDirectionY { get; private set; }
     public int Age { get; private set; }
-    public int WeaponDamage { get; private set; }
-    public float MovementSpeed { get; private set; }
+    private Movement Movement;
+    private Weapon Weapon;
 
     public void Move()
     {
@@ -21,10 +36,5 @@ class Player
     public void Attack()
     {
         //attack
-    }
-
-    public bool IsReloading()
-    {
-        throw new NotImplementedException();
     }
 }
