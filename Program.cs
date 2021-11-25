1973c328b729f47290e45a3b4319264b778fe666
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-11-25 03:39:20 +0300

    Task14 solution

--- Program.cs
+++ Program.cs
@@ -3,48 +3,106 @@
 
 // Проведите рефакторинг - https://gist.github.com/HolyMonkey/adcf9478bd6dcdd21384bf269155f8fe
 
-private void checkButton_Click(object sender, EventArgs e)
+// pasportNumber переведен из int в string ? т.к. Серия Паспорта может быть не только цифровая
+
+private SQLiteConnection ConnectSQLite(string connectionString)
+{
+    try
     {
-      if (this.passportTextbox.Text.Trim() == "")
-      {
-        int num1 = (int) MessageBox.Show("Введите серию и номер паспорта");
-      }
-      else
-      {
-        string rawData = this.passportTextbox.Text.Trim().Replace(" ", string.Empty);
-        if (rawData.Length < 10)
-        {
-          this.textResult.Text = "Неверный формат серии или номера паспорта";
-        }
-        else
-        {
-          string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object) Form1.ComputeSha256Hash(rawData));
-          string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
-          try
-          {
-            SQLiteConnection connection = new SQLiteConnection(connectionString);
-            connection.Open();
-            SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
-            DataTable dataTable1 = new DataTable();
-            DataTable dataTable2 = dataTable1;
-            sqLiteDataAdapter.Fill(dataTable2);
-            if (dataTable1.Rows.Count > 0)
-            {
-              if (Convert.ToBoolean(dataTable1.Rows[0].ItemArray[1]))
-                this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
-              else
-                this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
-            }
-            else
-              this.textResult.Text = "Паспорт «" + this.passportTextbox.Text + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
-            connection.Close();
-          }
-          catch (SQLiteException ex)
-          {
-            if (ex.ErrorCode != 1)
-              return;
-            int num2 = (int) MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
-          }
-        }
-      }
+        return connection = new SQLiteConnection(connectionString);
     }
+    catch (SQLiteException ex)
+    {
+        if (ex.ErrorCode == 1)
+        {
+            int num2 = (int) MessageBox.Show("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+        }
+        throw new InvalidOperationException("Не можем подсоединится к SQLite.");
+    }
+}   
+
+private string GetConnectionString()
+{
+    return string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
+}   
+
+private string GetCommand(string pasportNumber)
+{
+    //  TODO: limit 1 блокирует ситуацию, когда возможно придет 2 записи, а это ошибка
+    //  или num должен быть как ключевое поле таблицы passports. Проверить! 
+    return string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(pasportNumber));
+}   
+
+private bool PackPasportNumber(string pasportNumber)
+{
+    return pasportNumber.Trim().Replace(" ", string.Empty);
+}
+private bool IsValidPasportNumber(string pasportNumber)
+{
+    if (pasportNumber.Length < 10)
+    {
+        return false;
+    }
+    return true;
+}
+
+private int GetPasportNumber()
+{
+    string pasportNumber = PackPasportNumber(this.passportTextbox.Text);
+    if (pasportNumber == "")
+    {
+        pasportNumber = MessageBox.Show("Введите серию и номер паспорта");
+    }
+
+    return pasportNumber;
+}
+
+private bool? GetAccessRoleFromDB(string pasportNumber)
+{
+    SQLiteConnection connection = ConnectSQLite(GetConnectionString());
+    connection.Open();
+    SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(GetCommand(pasportNumber), connection));
+    DataTable dataTable = new DataTable();
+    sqLiteDataAdapter.Fill(dataTable);
+
+    // TODO: Нельзя брать ItemArray[1], т.к. в select * на первом месте может приходить другое поле. Нужно обращаться к конкретно именованному полю!
+    bool? result = dataTable.Rows.Count > 0 ? Convert.ToBoolean(dataTable.Rows[0].ItemArray[1]) : null;
+
+    connection.Close();
+
+    return result;
+}
+
+private string GetAccessRole(string pasportNumber)
+{
+    bool? role = GetAccessRoleFromDB(pasportNumber);
+    if (role == null)
+    {
+        return = $"Паспорт «{pasportNumber}» в списке участников дистанционного голосования НЕ НАЙДЕН";
+    }
+
+    if (role)
+    {
+        return $"По паспорту «{pasportNumber}» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+    }
+    else
+    {
+        return $"По паспорту «{pasportNumber}» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+    }
+}
+
+private string SetVouteAccessRole()
+{
+    string pasportNumber = GetPasportNumber();
+
+    if (IsValidPasportNumber(pasportNumber))
+        return "Неверный формат серии или номера паспорта";
+
+    return GetAccessRole(pasportNumber);
+}
+
+
+private void checkButton_Click(object sender, EventArgs e)
+{
+    this.textResult.Text = SetVouteAccessRole();
+}
