e8d9a81448ea71dfe18247ad20a28008889fb8fd
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-11-29 19:46:50 +0300

    Task16 solution

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+// ЗАДАЧА
+//
+// Попробуйте заменить метод на пару Включить\Выключить 
+// https://gist.github.com/HolyMonkey/ef0c234f158151d80ecb22d3717c9ac2
+
+public void SetPowerOn(bool enable)
+{
+   _enable = enable;
+   _effects.StartEnableAnimation();
+}
+
+public void SetPowerOff(bool enable)
+{
+   _enable = false;
+   _pool.Free(this);
+}
